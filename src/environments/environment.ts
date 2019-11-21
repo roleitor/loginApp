ce812d4739675106abc71ece6e316525066b3c23
roleitor <rolomen_20@hotmail.com>
2019-11-21 17:17:34 -0400

    Firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyByLoa1lb5vbVucY2pU27KpiwC7AwJkzuw",
+    authDomain: "loginapp-97193.firebaseapp.com",
+    databaseURL: "https://loginapp-97193.firebaseio.com",
+    projectId: "loginapp-97193",
+    storageBucket: "loginapp-97193.appspot.com",
+    messagingSenderId: "447462092681",
+    appId: "1:447462092681:web:f1bfe54e94a89f84ff3b58",
+    measurementId: "G-0FXVKDPXXH"
+  }
+
 };
 
 /*
