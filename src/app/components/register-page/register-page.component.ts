ef29421699c421346139fe5a7ac09471b484b9a2
roleitor <rolomen_20@hotmail.com>
2019-11-22 10:56:54 -0400

    Creado el servicio de AUTH y el registro de usuarios

--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  public email:string;
+  public password:string;
+  constructor(public authService:AuthService, public router:Router) { 
+  }
 
   ngOnInit() {
+  }
+
+  onSubmitAddUser(){
+    this.authService.registerUser(this.email,this.password).then((res)=>{
+      this.router.navigate(['/privado']);
+    }).catch((err)=>{
+      console.log(err);
+    });
   }
 
 }
